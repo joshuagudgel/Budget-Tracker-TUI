a254e589a27e950a6b69b6a9ae75583f63d7dbb1
Josh <joshuabgudgel@gmail.com>
2026-02-28 10:56:35 -0800

    Update app name

--- build.bat
+++ build.bat
@@ -7,22 +7,22 @@ if not exist dist mkdir dist
 echo Building for Windows...
 set GOOS=windows
 set GOARCH=amd64
-go build -o dist\budget-tracker-windows.exe .
+go build -o dist\finances-wrapped-windows.exe .
 
 echo Building for Linux...
 set GOOS=linux
 set GOARCH=amd64
-go build -o dist\budget-tracker-linux .
+go build -o dist\finances-wrapped-linux .
 
 echo Building for macOS Intel...
 set GOOS=darwin
 set GOARCH=amd64
-go build -o dist\budget-tracker-macos-intel .
+go build -o dist\finances-wrapped-macos-intel .
 
 echo Building for macOS ARM...
 set GOOS=darwin
 set GOARCH=arm64
-go build -o dist\budget-tracker-macos-arm .
+go build -o dist\finances-wrapped-macos-arm .
 
 echo Done! Executables are in the dist folder.
 pause
